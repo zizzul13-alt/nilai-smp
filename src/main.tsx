c6206b854c898adf42b7289dc9d8517e252620e6
zizzul13-alt <z.izzul13@gmail.com>
2026-09-06 16:57:33 +0700

    R3.5-01: load reporting styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './app/App';
 import './styles/base.css';
+import './styles/reporting.css';
 
 const root = document.getElementById('root');
 
