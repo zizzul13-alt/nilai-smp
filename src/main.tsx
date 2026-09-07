e4fe8cf4edf1add9c26fe592f220086fe507c97a
zizzul13-alt <z.izzul13@gmail.com>
2026-09-07 11:47:07 +0700

    R3.7-01 load daily driver styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './app/App';
 import './styles/base.css';
+import './styles/daily-driver.css';
 import './styles/reporting.css';
 import './styles/artifacts.css';
 
